945b70d889f7c9825a850d14c7f2cc74e18ca81a
Juan Duque <juanmanuelduque0920@gmail.com>
2022-10-25 16:49:18 -0500

    Subir el deploy

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const {
 } = process.env;
 
 // Syncing all the models at once.
+
+// Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
