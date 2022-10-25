50b058b2f79d84c62892fbcc8bb441f734feaa97
Juan Duque <juanmanuelduque0920@gmail.com>
2022-10-25 17:00:58 -0500

    Subir el deploy 2

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const {
   PORT
 } = process.env;
 
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,DB
+} = process.env;
+
+console.log({DB_USER}, {DB_PASSWORD}, {DB_HOST},{DB})
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
